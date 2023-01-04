4a160a6465af094a4c182916ac54eb066bc3d3ea
Shreya-CK <60349224+Shreya-CK@users.noreply.github.com>
2023-01-05 00:25:58 +0530

    Delete Downloads/sts-4.14.1.RELEASE/self directory

--- Downloads/sts-4.14.1.RELEASE/self/src/main/java/com/emp/service/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package com.emp.service;
-
-import java.util.List;
-import com.emp.model.Employee;
-public interface EmployeeService {
-public List<Employee> getAllEmployee();
-public List<Employee> getEmployeeByDept(String department);
-}
